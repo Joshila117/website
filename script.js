28021fac585acf0f09bebd500d29b4ff369cea44
Joshila117 <joshila0711@gmail.com>
2023-09-12 23:09:02 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function showContent(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      section.style.display = 'none';
+    });
+  
+    // Show the selected section
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.style.display = 'block';
+  }
+  
